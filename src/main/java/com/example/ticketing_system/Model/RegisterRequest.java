a2719f8c0c8df8e523e58d4aa700026c2e8d61c6
Zain <zainhu06@hotmail.com>
2026-08-24 14:15:30 +0100

    Added model / fixes

---
+++ src/main/java/com/example/ticketing_system/Model/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.example.ticketing_system.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+}
